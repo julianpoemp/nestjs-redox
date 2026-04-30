e6c343db7a62513fb568700c30ce6660d5ca560e
Julian Pömp <jp@poemp.net>
2026-04-30 20:06:02 +0200

    build(nestjs-redox): update Nx to v22.7.1

--- apps/demo-express/jest.config.ts
+++ apps/demo-express/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'demo-express',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
--- apps/demo-fastify/jest.config.ts
+++ apps/demo-fastify/jest.config.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-export default {
+module.exports = {
   displayName: 'demo-fastify-config',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
